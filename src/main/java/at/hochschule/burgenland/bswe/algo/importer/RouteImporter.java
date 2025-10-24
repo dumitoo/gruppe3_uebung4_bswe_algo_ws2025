3b666cf0907d0da188fcfd3c0f0e799e47c115ba
Tobias Thurner <2410859009@hochschule-burgenland.at>
2025-10-24 20:39:53 +0200

    Added RouteImporter

---
+++ src/main/java/at/hochschule/burgenland/bswe/algo/importer/RouteImporter.java
@@ -0,0 +1,41 @@
+package at.hochschule.burgenland.bswe.algo.importer;
+
+import at.hochschule.burgenland.bswe.algo.entities.Route;
+import at.hochschule.burgenland.bswe.algo.util.CSVUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RouteImporter implements Importable {
+    private static final Logger log = LogManager.getLogger(RouteImporter.class);
+
+    @Override
+    public List<Object> importData() {
+        List<Object> routes = new ArrayList<>();
+
+        CSVUtils.readCSV(Paths.get("src", "main", "resources", "routes.csv")).forEach(line -> {
+            try {
+                String[] splittedLine = line.replace(" ", "").split(",");
+                int splittedLineLength = splittedLine.length;
+                if (splittedLineLength != 5) {
+                    throw new IllegalArgumentException("Falsche Anzahl an Parametern (" + splittedLineLength + " statt 5)!");
+                }
+                int id = Integer.parseInt(splittedLine[0]);
+                List<Integer> flights = Arrays.stream(splittedLine[1].split("-")).map(Integer::parseInt).toList();
+                int totalDuration = Integer.parseInt(splittedLine[2]);
+                double totalPrice = Double.parseDouble(splittedLine[3]);
+                int stopovers = Integer.parseInt(splittedLine[4]);
+
+                routes.add(new Route(id, flights, totalDuration, totalPrice, stopovers));
+            } catch (IllegalArgumentException | NullPointerException e) {
+                log.error("Fehler beim Importieren der Route: " + e.getLocalizedMessage());
+            }
+        });
+
+        return routes;
+    }
+}
