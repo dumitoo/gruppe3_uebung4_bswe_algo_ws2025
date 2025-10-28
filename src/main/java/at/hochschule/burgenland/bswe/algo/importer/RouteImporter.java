a0de442b55a268479cd1006a7ef3c1e4cb8bb0a6
Tobias Thurner <2410859009@hochschule-burgenland.at>
2025-10-28 20:04:36 +0100

    Adapted RouteImporter to store Flights instead of Integers

--- src/main/java/at/hochschule/burgenland/bswe/algo/importer/RouteImporter.java
+++ src/main/java/at/hochschule/burgenland/bswe/algo/importer/RouteImporter.java
@@ -1,20 +1,22 @@
 package at.hochschule.burgenland.bswe.algo.importer;
 
+import at.hochschule.burgenland.bswe.algo.entities.Flight;
 import at.hochschule.burgenland.bswe.algo.entities.Route;
 import at.hochschule.burgenland.bswe.algo.util.CSVUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class RouteImporter implements Importable<Route> {
     private static final Logger log = LogManager.getLogger(RouteImporter.class);
 
     @Override
     public List<Route> importData() {
+        Map<Integer, Flight> flightMap = new HashMap<>();
+        new FlightsImporter().importData().forEach(flight -> flightMap.put(flight.getId(), flight));
+
         List<Route> routes = new ArrayList<>();
 
         CSVUtils.readCSV(Paths.get("src", "main", "resources", "routes.csv")).forEach(line -> {
@@ -25,12 +27,12 @@ public class RouteImporter implements Importable<Route> {
                     throw new IllegalArgumentException("Falsche Anzahl an Parametern (" + splittedLineLength + " statt 5)!");
                 }
                 int id = Integer.parseInt(splittedLine[0]);
-                List<Integer> flights = Arrays.stream(splittedLine[1].split("-")).map(Integer::parseInt).toList();
+                List<Flight> flights = Arrays.stream(splittedLine[1].split("-")).map(Integer::parseInt).map(flightMap::get).toList();
                 int totalDuration = Integer.parseInt(splittedLine[2]);
                 double totalPrice = Double.parseDouble(splittedLine[3]);
                 int stopovers = Integer.parseInt(splittedLine[4]);
 
-                //routes.add(new Route(id, flights, totalDuration, totalPrice, stopovers)); TODO: map id to flight
+                routes.add(new Route(id, flights, totalDuration, totalPrice, stopovers));
             } catch (IllegalArgumentException | NullPointerException e) {
                 log.error("Fehler beim Importieren der Route: " + e.getLocalizedMessage());
             }
